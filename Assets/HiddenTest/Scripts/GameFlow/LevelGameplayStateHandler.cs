0a249f5e0d52e8cec08271028414301a712ae4e2
KarenKrill <vampart1997@gmail.com>
2025-12-08 09:05:59 +0500

    Added stub state handlers

---
+++ Assets/HiddenTest/Scripts/GameFlow/LevelGameplayStateHandler.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using KarenKrill.UniCore.StateSystem.Abstractions;
+
+namespace HiddenTest.GameFlow
+{
+    using Abstractions;
+
+    public class LevelGameplayStateHandler : PresentableStateHandlerBase<GameState>, IStateHandler<GameState>
+    {
+        public override GameState State => GameState.LevelGameplay;
+
+        public LevelGameplayStateHandler()
+        {
+        }
+
+        public override void Enter(GameState prevState, object? context = null)
+        {
+            base.Enter(prevState);
+        }
+
+        public override void Exit(GameState nextState)
+        {
+            base.Exit(nextState);
+        }
+    }
+}
---
+++ Assets/HiddenTest/Scripts/GameFlow/LoadingStateHandler.cs
@@ -0,0 +1,26 @@
+﻿#nullable enable
+using KarenKrill.UniCore.StateSystem.Abstractions;
+
+namespace HiddenTest.GameFlow
+{
+    using Abstractions;
+
+    public class LoadingStateHandler : PresentableStateHandlerBase<GameState>, IStateHandler<GameState>
+    {
+        public override GameState State => GameState.Loading;
+
+        public LoadingStateHandler()
+        {
+        }
+
+        public override void Enter(GameState prevState, object? context = null)
+        {
+            base.Enter(prevState);
+        }
+
+        public override void Exit(GameState nextState)
+        {
+            base.Exit(nextState);
+        }
+    }
+}
