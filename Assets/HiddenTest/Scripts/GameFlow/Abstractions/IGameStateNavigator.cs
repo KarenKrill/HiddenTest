fdfe12ea52825763281fbf5222124c03e4c61d05
KarenKrill <vampart1997@gmail.com>
2025-12-08 14:45:08 +0500

    Added game state navigator

---
+++ Assets/HiddenTest/Scripts/GameFlow/Abstractions/IGameStateNavigator.cs
@@ -0,0 +1,12 @@
+﻿namespace HiddenTest.GameFlow.Abstractions
+{
+    public interface IGameStateNavigator
+    {
+        GameState CurrentState { get; }
+
+        void LoadMainMenu();
+        void LoadLevel(int levelId);
+        void FinishLevel();
+        void Exit();
+    }
+}
