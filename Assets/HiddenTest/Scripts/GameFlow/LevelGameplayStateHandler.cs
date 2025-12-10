4c55f78c7d87b2d58a35a013d8260066491d5450
KarenKrill <vampart1997@gmail.com>
2025-12-11 03:56:02 +0500

    Added hidden item interactions in gameplay state handler

--- Assets/HiddenTest/Scripts/GameFlow/LevelGameplayStateHandler.cs
+++ Assets/HiddenTest/Scripts/GameFlow/LevelGameplayStateHandler.cs
@@ -1,5 +1,9 @@
 ﻿#nullable enable
+using System;
+using System.Linq;
+using UnityEngine;
 using KarenKrill.UniCore.StateSystem.Abstractions;
+using HiddenTest.Gameplay.Abstractions;
 using HiddenTest.UI.Presenters.Abstractions;
 
 namespace HiddenTest.GameFlow
@@ -10,19 +14,54 @@ namespace HiddenTest.GameFlow
     {
         public LevelGameplayStateHandler(IGameStateNavigator gameStateNavigator,
             IInGameMenuPresenter inGameMenuPresenter,
-            IPauseMenuPresenter pauseMenuPresenter) :
+            IPauseMenuPresenter pauseMenuPresenter,
+            IGameConfig gameConfig,
+            ILevelItemsRegistry levelItemsRegistry) :
             base(gameStateNavigator, inGameMenuPresenter, pauseMenuPresenter)
         {
+            _gameConfig = gameConfig;
+            _levelItemsRegistry = levelItemsRegistry;
+            _levelItemsRegistry.ItemRegistered += OnLevelItemRegistered;
+            _levelItemsRegistry.ItemUnregistered += OnLevelItemUnregistered;
         }
 
         public override void Enter(GameState prevState, object? context = null)
         {
             base.Enter(prevState);
+            if (context is LevelLoadContext levelLoadContext && levelLoadContext.LevelIndex >= 0)
+            {
+                _levelConfig = _gameConfig.LevelsConfig[levelLoadContext.LevelIndex];
+            }
+            else
+            {
+                throw new ArgumentException($"Can't start {nameof(GameState.LevelGameplay)} without {nameof(LevelLoadContext)} context", nameof(context));
+            }
         }
 
         public override void Exit(GameState nextState)
         {
             base.Exit(nextState);
+        }
+
+        private readonly IGameConfig _gameConfig;
+        private ILevelConfig? _levelConfig;
+        private readonly ILevelItemsRegistry _levelItemsRegistry;
+
+        private void OnLevelItemRegistered(int id, IHiddenObject hiddenObject)
+        {
+            hiddenObject.Clicked += OnHiddenObjectClicked;
+        }
+        private void OnLevelItemUnregistered(int id, IHiddenObject hiddenObject)
+        {
+            hiddenObject.Clicked -= OnHiddenObjectClicked;
+        }
+        private void OnHiddenObjectClicked(IHiddenObject hiddenObject)
+        {
+            var itemConfig = _levelConfig?.Items.FirstOrDefault(item => item.Id == hiddenObject.Config.Id);
+            if (itemConfig != null)
+            {
+                Debug.Log($"{itemConfig.Name} clicked");
+            }
         }
     }
 }
