929cb9c9b9c9a4ac0dedf10fe53b6fe43bc901bc
KarenKrill <vampart1997@gmail.com>
2025-12-12 06:29:14 +0500

    Register only enabled items

--- Assets/HiddenTest/Scripts/Gameplay/LevelItemsRegistry.cs
+++ Assets/HiddenTest/Scripts/Gameplay/LevelItemsRegistry.cs
@@ -13,10 +13,19 @@ namespace HiddenTest.Gameplay
         public event Action<int, IHiddenObject>? ItemRegistered;
         public event Action<int, IHiddenObject>? ItemUnregistered;
 
+        public LevelItemsRegistry(IGameConfig gameConfig, ILevelSession levelSession)
+        {
+            _gameConfig = gameConfig;
+            levelSession.ActiveLevelChanged += OnActiveLevelChanged;
+        }
+
         public void Register(int id, IHiddenObject hiddenObject)
         {
-            _items[id] = hiddenObject;
-            ItemRegistered?.Invoke(id, hiddenObject);
+            if (_currentLevelConfig?.EnabledItems.ContainsKey(id) ?? false)
+            {
+                _items[id] = hiddenObject;
+                ItemRegistered?.Invoke(id, hiddenObject);
+            }
         }
         public void Unregister(int id)
         {
@@ -26,6 +35,20 @@ namespace HiddenTest.Gameplay
             }
         }
 
+        private readonly IGameConfig _gameConfig;
         private readonly Dictionary<int, IHiddenObject> _items = new();
+        private ILevelConfig? _currentLevelConfig;
+
+        private void OnActiveLevelChanged(int levelIndex)
+        {
+            if (levelIndex >= 0 && levelIndex < _gameConfig.LevelsConfig.Count)
+            {
+                _currentLevelConfig = _gameConfig.LevelsConfig[levelIndex];
+            }
+            else
+            {
+                _currentLevelConfig = null;
+            }
+        }
     }
 }
