e8cc58c361c527b3d838232485ef4e99c9078e11
KarenKrill <vampart1997@gmail.com>
2025-12-12 06:26:47 +0500

    Added state changing events

--- Assets/HiddenTest/Scripts/GameFlow/Abstractions/IGameStateNavigator.cs
+++ Assets/HiddenTest/Scripts/GameFlow/Abstractions/IGameStateNavigator.cs
@@ -1,8 +1,16 @@
-﻿namespace HiddenTest.GameFlow.Abstractions
+﻿#nullable enable
+
+namespace HiddenTest.GameFlow.Abstractions
 {
+    public delegate void StateChangingHandler(GameState fromState, GameState toState);
+    public delegate void StateChangedHandler(GameState state);
+
     public interface IGameStateNavigator
     {
         GameState State { get; }
+
+        event StateChangingHandler? StateChanging;
+        event StateChangedHandler? StateChanged;
 
         void LoadMainMenu();
         void LoadLevel(int levelId);
